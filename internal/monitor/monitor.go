45d0f89920cc398032a497931f07fbfc22dd1ce4
Devin Collins <me@imdevinc.com>
2019-10-16 15:04:06 -0600

    Fixed streaming and regex parsing

--- internal/monitor/monitor.go
+++ internal/monitor/monitor.go
@@ -1,13 +1,10 @@
 package monitor
 
 import (
+	"regexp"
+
 	"github.com/dghubble/go-twitter/twitter"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/clientcredentials"
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
+	"github.com/dghubble/oauth1"
 )
 
 type MonitorClient struct {
@@ -15,13 +12,10 @@ type MonitorClient struct {
 	stream *twitter.Stream
 }
 
-func NewTwitterClient(consumerKey string, consumerSecret string) MonitorClient {
-	config := &clientcredentials.Config{
-		ClientID:     consumerKey,
-		ClientSecret: consumerSecret,
-		TokenURL:     "https://api.twitter.com/oauth2/token",
-	}
-	httpClient := config.Client(oauth2.NoContext)
+func NewTwitterClient(consumerKey string, consumerSecret string, accessToken string, accessSecret string) MonitorClient {
+	config := oauth1.NewConfig(consumerKey, consumerSecret)
+	token := oauth1.NewToken(accessToken, accessSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
 	client := twitter.NewClient(httpClient)
 	return MonitorClient{client, nil}
 }
@@ -36,40 +30,46 @@ func (c *MonitorClient) SearchTweets(query string) (*twitter.Search, error) {
 	return tweets, nil
 }
 
-func (c *MonitorClient) StopStream() {
+func (c *MonitorClient) StopMonitor() {
 	if c.stream != nil {
 		c.stream.Stop()
 	}
 }
 
-func (c *MonitorClient) StartMonitor() {
-	log.Println("Starting monitor...")
+func (c *MonitorClient) StartMonitor() error {
 	demux := twitter.NewSwitchDemux()
 	demux.Tweet = func(tweet *twitter.Tweet) {
-		log.Println(tweet)
+		processTweet(tweet.Text)
 	}
 
-	// filterParams := &twitter.StreamFilterParams{
-	// 	Follow:         []string{"@DuvalMagic"},
-	// 	StallWarnings: twitter.Bool(true),
-	// }
-
-	// c.stream, err = c.client.Streams.Filter(filterParams)
 	var err error
-	c.stream, err = c.client.Streams.Sample(&twitter.StreamSampleParams{
+	filterParams := &twitter.StreamFilterParams{
+		Follow:        []string{"8369072"},
 		StallWarnings: twitter.Bool(true),
-	})
+	}
+
+	c.stream, err = c.client.Streams.Filter(filterParams)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	go demux.HandleChan(c.stream.Messages)
 
-	log.Println("Waiting for stream end...")
-	ch := make(chan os.Signal)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	log.Println(<-ch)
+	return nil
+}
 
-	log.Println("Stopping stream...")
-	c.StopStream()
+func processTweet(input string) (bool, []string) {
+	codes := []string{}
+	found := false
+
+	re := regexp.MustCompile(`(\w{5}\-){4}\w{5}`)
+	matches := re.FindAll([]byte(input), -1)
+	if matches != nil {
+		found = true
+		for _, code := range matches {
+			codes = append(codes, string(code))
+		}
+	}
+
+	return found, codes
 }
--- internal/monitor/monitor_test.go
+++ internal/monitor/monitor_test.go
@@ -1,19 +1,44 @@
 package monitor
 
 import (
-	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
+const sampleTweet = `
+Sunday, Bloody Sunday. SHiFT code for a Golden Key for Borderlands 3: 
+
+CZCTJ-CZ59T-HC35W-T3BJB-ZTZJC
+
+Active until 10pm CST.
+
+Redeem in game or via http://shift.gearboxsoftware.com  - Collect Golden Key from in-game mail in “Social” menu. 
+
+Happy Vault Hunting!
+`
+const sampleCode = "CZCTJ-CZ59T-HC35W-T3BJB-ZTZJC"
+
 func TestNewTwitterClient(t *testing.T) {
-	client := NewTwitterClient(os.Getenv("TWITTER_KEY"), os.Getenv("TWITTER_SECRET"))
+	client := NewTwitterClient(os.Getenv("CONSUMER_KEY"), os.Getenv("CONSUMER_SECRET"), os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_SECRET"))
 	tweets, err := client.SearchTweets("@DuvalMagic")
 	assert.Nil(t, err, err)
 	assert.NotEmpty(t, tweets.Statuses, "Tweets shouldn't be empty")
 }
 
 func TestTwitterStream(t *testing.T) {
-	client := NewTwitterClient(os.Getenv("TWITTER_KEY"), os.Getenv("TWITTER_SECRET"))
-	client.StartMonitor()
+	client := NewTwitterClient(os.Getenv("CONSUMER_KEY"), os.Getenv("CONSUMER_SECRET"), os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_SECRET"))
+	err := client.StartMonitor()
+	assert.Nil(t, err, err)
+	time.Sleep(10 * time.Second)
+	client.StopMonitor()
+}
+
+func TestTweetProcessing(t *testing.T) {
+	found, codes := processTweet(sampleTweet)
+	assert.True(t, found, "Found should be true")
+	assert.Len(t, codes, 1, "Should only be one code")
+	assert.Equal(t, "CZCTJ-CZ59T-HC35W-T3BJB-ZTZJC", codes[0], "Code %s does not match %s", codes[0], sampleCode)
 }
